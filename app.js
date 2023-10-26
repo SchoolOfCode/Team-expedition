5f3dac10b658299e6a0a2b4a56a1c8180ebbc173
Cal Woodford <callum@woodford.info>
2023-10-26 14:40:51 +0100

    removed: database_connect.js

--- app.js
+++ app.js
@@ -10,5 +10,21 @@ app.use(cors());
 
 // import { topicsRoutes } from "./routes/topicsRoutes.js";
 
+// For the icebreaker connection
 app.use("/icebreaker", icebreakerRoutes);
 //app.use("/topics", topicsRoutes);
+
+//For retrieving questions from the Postgres database
+app.get("/questions/id", async function (req, res) {
+    const id = req.params.id;
+    const questions = await getQuestionById(id);
+   ​
+    if (!questions) {
+       return res
+       .status(404)
+       .json ({ status: "fail", data: { msg: "Questions not found"}});
+   ​
+    }
+    res.status(200).json({status: "Success", data: questions});
+   ​
+   });
