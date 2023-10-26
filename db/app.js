0202cb08982408df4bac1cd980ba0eec0e95dd7f
Cal Woodford <callum@woodford.info>
2023-10-26 14:50:02 +0100

    added: db directory

--- db/app.js
+++ db/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { icebreakerRoutes } from "./routes/icebreakerRoutes.js";
+import { icebreakerRoutes } from "../routes/icebreakerRoutes.js";
+import { pool } from "pg";
 
 export const app = express();
 
@@ -14,15 +15,23 @@ app.use(cors());
 app.use("/icebreaker", icebreakerRoutes);
 //app.use("/topics", topicsRoutes);
 
+
+export async function getQuestionsById(id) {
+
+    const queryText = "SELECT * FROM questions WHERE id = $1";
+    const result = await pool.query(question, [id]);
+    return result.rows[0] || null;
+
+  }
+
 //For retrieving questions from the Postgres database
-app.get("/questions/id", async function (req, res) {
+app.get("/question/id", async function (req, res) {
     const id = req.params.id;
-    const questions = await getQuestionById(id);
-   ​
+    const questions = await getQuestionsById(id);
     if (!questions) {
        return res
        .status(404)
-       .json ({ status: "fail", data: { msg: "Questions not found"}});
+       .json ({ status: "fail", data: { msg: "Question not found"}});
    ​
     }
     res.status(200).json({status: "Success", data: questions});
--- db/server.js
+++ db/server.js
@@ -1,4 +1,4 @@
-import { app } from "./app.js";
+import { app } from "./db/app.js";
 
 const PORT = 4000;
 
